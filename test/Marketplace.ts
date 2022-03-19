26c438acc1035f221bcf59f96abd890f808685d9
hostlime <hostlime@ya.ru>
2022-03-19 16:00:17 +0300

    add test

--- test/Marketplace.ts
+++ test/Marketplace.ts
@@ -2,23 +2,26 @@ import { expect, assert } from "chai";
 import { ethers } from "hardhat";
 
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { Console } from "console";
 
 
 describe("Marketplace", function () {
     let owner: SignerWithAddress;
-    let addr1: SignerWithAddress;
-    let addr2: SignerWithAddress;
+    let seller: SignerWithAddress;
+    let buyer: SignerWithAddress;
     let addrs: SignerWithAddress[];
 
     let uriNFT = "https://gateway.pinata.cloud/ipfs/QmWWXb1n1avqmxfQpS3htjgFi7rufJYBpHCTUE9UEbhq3f";
-
+    let tokenIdNFT;
+    let costNft = 100;
+    let balanceAccount = 1000;
     let marketplace: any;
     let token: any;
     let nft: any;
 
     // создаём экземпляр контрактов
     beforeEach(async () => {
-        [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+        [owner, seller, buyer, ...addrs] = await ethers.getSigners();
         // NFT
         const Nft = await ethers.getContractFactory("TokenERC721");
         nft = await Nft.deploy();
@@ -30,12 +33,23 @@ describe("Marketplace", function () {
         // Контракт МАРКЕТПЛЕЙСА
         const Marketplace = await ethers.getContractFactory("Marketplace");
         marketplace = await Marketplace.deploy(nft.address, token.address);
-        await nft.deployed();
+        await marketplace.deployed();
 
         // Назначаем роль минтера nft для контракта маркетплейса
         const MARKET_ROLE = await nft.MARKET_ROLE();
         await nft.grantRole(MARKET_ROLE, marketplace.address);
 
+        // Минтим NFT
+        tokenIdNFT = (await marketplace.connect(seller).createItem(uriNFT)).value.toNumber();
+        // Апрувим NFT для контракта
+        await nft.connect(seller).approve(marketplace.address, tokenIdNFT);
+
+        // Даем токены erc20 покупателю и продавцу
+        await token.connect(owner).transfer(seller.address, balanceAccount);
+        await token.connect(owner).transfer(buyer.address, balanceAccount);
+        // разрешаем контракту списывать токены
+        await token.connect(seller).approve(marketplace.address, balanceAccount);
+        await token.connect(buyer).approve(marketplace.address, balanceAccount);
     });
 
     // проверка, что контракт NFT задеплоен
@@ -59,13 +73,38 @@ describe("Marketplace", function () {
     });
 
     it.only('Checking function createItem()', async () => {
-        const _tokenID = await marketplace.createItem(uriNFT);
-        expect(await nft.balanceOf(owner.address)).to.be.equal(1)
+        expect(await nft.balanceOf(seller.address)).to.be.equal(1)
         // Убеждаемся что урл есть под id = 0
         expect(await nft.tokenURI(0)).to.have.string(uriNFT)
         // Убеждаемся что nft принадлежит овнеру
-        expect(await nft.ownerOf(0)).to.be.equal(owner.address)
+        expect(await nft.ownerOf(0)).to.be.equal(seller.address)
     });
+    it.only('Checking function listItem()', async () => {
+        // Убеждаемся что только владелец может выставить на аукцион 
+        await expect(marketplace.connect(buyer).listItem(0, costNft)).to.be.revertedWith(
+            "Only owner NFT can call this function widt that _tokenID"
+        );
+        const Event = await marketplace.connect(seller).listItem(0, costNft);
+        // Проверка эвента
+        expect(Event).to.emit(marketplace, "ListItem")
+            .withArgs(seller.address, 0, costNft)
+        // Проверка выставленного лота в мапинге
+        expect((await marketplace.sListItem(0)).vendor).to.be.equal(seller.address)
+        expect((await marketplace.sListItem(0)).price).to.be.equal(costNft)
+    });
+    it.only('Checking function buyItem()', async () => {
+        // Убеждаемся что нельзя купить несуществующий лот
+        await expect(marketplace.connect(buyer).buyItem(0)).to.be.revertedWith(
+            "NFT does not exist"
+        );
+        // выставляем NFT на продажу
+        await marketplace.connect(seller).listItem(0, costNft);
+        // Убеждаемся что владелец не может покупать
+        await expect(marketplace.connect(seller).buyItem(0)).to.be.revertedWith(
+            "Owner cant buy his nft"
+        );
 
+
+    });
 
 });
